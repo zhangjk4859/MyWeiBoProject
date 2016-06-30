f49174a8326d698fe13531ac316cae037c30f6c0
“kevin” <zhangjk4859@163.com>
2016-06-30 12:07:47 +0800

    为首页添加了弹出视图

---
+++ sinaWeibo/sinaWeibo/Classes/Home/Popover/JKPopoverPC.swift
@@ -0,0 +1,13 @@
+//
+//  JKPopoverPC.swift
+//  sinaWeibo
+//
+//  Created by 张俊凯 on 6/30/16.
+//  Copyright © 2016 张俊凯. All rights reserved.
+//
+
+import UIKit
+
+class JKPopoverPC: UIPresentationController {
+
+}
---
+++ sinaWeibo/sinaWeibo/Classes/Home/Popover/JKPopoverViewController.swift
@@ -0,0 +1,13 @@
+//
+//  JKPopoverViewController.swift
+//  sinaWeibo
+//
+//  Created by 张俊凯 on 6/30/16.
+//  Copyright © 2016 张俊凯. All rights reserved.
+//
+
+import Cocoa
+
+class JKPopoverViewController: UIViewController {
+
+}
