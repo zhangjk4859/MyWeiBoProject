f417e9ce21ee1a2d779727830c538e0a5ae30986
“kevin” <zhangjk4859@163.com>
2016-06-30 16:28:12 +0800

    二维码增加了动画

--- sinaWeibo/sinaWeibo/Classes/Home/QRCode/JKQRCodeVC.swift
+++ sinaWeibo/sinaWeibo/Classes/Home/QRCode/JKQRCodeVC.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class JKQRCodeVC: UIViewController {
+class JKQRCodeVC: UIViewController,UITabBarDelegate {
 
+    //容器视图高度
+    @IBOutlet weak var containerHeightCons: NSLayoutConstraint!
+    //冲击波视图
+    @IBOutlet weak var scanLineView: UIImageView!
+    
+    //冲击波视图顶部约束
+    @IBOutlet weak var scanLineCons: NSLayoutConstraint!
+    
+    
     //关闭按钮点击事件
     @IBAction func closeBtnClick(sender: UIBarButtonItem) {
     
@@ -19,6 +28,45 @@ class JKQRCodeVC: UIViewController {
 
     @IBOutlet weak var customTabBar: UITabBar!
     override func viewDidLoad() {
+         super.viewDidLoad()
         customTabBar.selectedItem = customTabBar.items![0]
+        customTabBar.delegate = self
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        startAnimation()
+    }
+    
+    func startAnimation(){
+        //让约束从顶部开始
+        self.scanLineCons.constant = -self.containerHeightCons.constant
+        self.scanLineView.layoutIfNeeded()
+        
+        //执行冲击波动画
+        UIView.animateWithDuration(2.0,animations: { () -> Void in
+            self.scanLineCons.constant = self.containerHeightCons.constant
+            //设置动画指定的次数
+            UIView.setAnimationRepeatCount(MAXFLOAT)
+            //强制更新画面
+            self.scanLineView.layoutIfNeeded()
+        })
+    }
+    
+    //MARK: - UITabBarDelagate
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        print(item.tag)
+        if item.tag == 1 {
+            self.containerHeightCons.constant = 300
+        }else{
+            self.containerHeightCons.constant = 150
+        }
+        
+        //停止动画
+        self.scanLineView.layer.removeAllAnimations()
+        
+        //重新开始动画
+        startAnimation()
     }
 }
