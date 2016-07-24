cb7758c0102f7e66e57fa04aec4c3f530f68d55b
zhangjk4859 <573581325@qq.com>
2016-07-24 23:40:26 +0800

    添加了图片浏览器

---
+++ sinaWeibo/sinaWeibo/Classes/Home/图片控制器/JKPictureVC.swift
@@ -0,0 +1,81 @@
+//
+//  JKPictureVC.swift
+//  sinaWeibo
+//
+//  Created by 张俊凯 on 16/7/24.
+//  Copyright © 2016年 张俊凯. All rights reserved.
+//
+
+import UIKit
+
+class JKPictureVC: UIViewController {
+
+    var currentIndex: Int?
+    var pictureURLs: [NSURL]?
+    init(index: Int, urls: [NSURL])
+    {
+        // Swift必须先初始化自身属性, 再初始化父类
+        currentIndex = index
+        pictureURLs = urls
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.whiteColor()
+        // 初始化UI
+        setupUI()
+    }
+    
+    private func setupUI(){
+        
+        // 1.添加子控件
+        view.addSubview(collectionView)
+        view.addSubview(closeBtn)
+        view.addSubview(saveBtn)
+        
+        // 2.布局子控件
+        closeBtn.jk_AlignInner(type: JK_AlignType.BottomLeft, referView: view, size: CGSize(width: 100, height: 35), offset: CGPoint(x: 10, y: -10))
+        saveBtn.jk_AlignInner(type: JK_AlignType.BottomRight, referView: view, size: CGSize(width: 100, height: 35), offset: CGPoint(x: -10, y: -10))
+        collectionView.frame = UIScreen.mainScreen().bounds
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func close()
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func save()
+    {
+        print(#function)
+    }
+    
+    // MARK: - 懒加载
+    private lazy var closeBtn: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("关闭", forState: UIControlState.Normal)
+        btn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        btn.backgroundColor = UIColor.darkGrayColor()
+        
+        btn.addTarget(self, action: #selector(JKPictureVC.close), forControlEvents: UIControlEvents.TouchUpInside)
+        return btn
+    }()
+    
+    private lazy var saveBtn: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("保存", forState: UIControlState.Normal)
+        btn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        btn.backgroundColor = UIColor.darkGrayColor()
+        
+        btn.addTarget(self, action: #selector(JKPictureVC.save), forControlEvents: UIControlEvents.TouchUpInside)
+        return btn
+    }()
+    
+    private lazy var collectionView: UICollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    
+}
